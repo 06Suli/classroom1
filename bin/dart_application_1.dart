56b9fccf3d030ce78b49d4ee063c9a52132a5c21
admin <admin@admins-MacBook-Pro.local>
2022-12-06 14:29:38 +0600

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,141 @@
+
+
+
+
+
+void main() {
+// double b = 4.20;
+// int a = 3; 
+// print(a + b);
+
+// String text = 'name';
+// text = 'Fultter';
+// print('i want to learn $text');
+
+// double myNumber = 2.225;
+// String myText = myNumber.toString();
+// print(myText);
+
+// int shokolad = 50;
+// int milk = 75;
+// int kofe = 40;
+
+// int summa;
+
+// summa = shokolad + milk + kofe;
+// print(summa);
+
+// int  a = 20;
+// int b ;
+// int c;
+
+// b = a + 3;
+
+// c = b + a;
+
+// print(a);
+// print(b);
+// print(c);
+
+
+// int cup = 5;
+// int saucer = 5;
+// int spoons = 5;
+
+// print(cup + saucer + spoons);
+
+ List ints = ['123123',2.0,'String',false,2];
+String a ='String';
+
+List listTwo = [1 ,23, 4, 56,];
+ints.add(a);
+
+// ints.addAll(listTwo);
+//  ints.insert(5, 'Start');
+// ints.insert(0, listTwo);
+
+// ints.add(listTwo);
+// ints.remove('123123');
+// ints.removeLast();
+
+// ints.removeRange(0, 3);
+
+// print(ints.contains);
+// ints.removeRange(0, 2);
+// print(ints);
+// ints.contains('123');
+//print(123);
+
+// listTwo.sort();
+
+// Set setOne = {
+// 1,
+// 2,
+// 3,
+// 4,
+// 5,
+// 6,
+
+// }
+// print(setOne);
+
+Map<String, int> noteBook ={
+'Suli' : 0703694912,
+'Arsen' : 0434805885,
+
+};
+
+print(noteBook);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print(ints);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  } 
